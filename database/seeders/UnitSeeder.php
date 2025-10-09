1a6e78b61abb3cb733b4940d94ff638daa6a45c7
Naeyla <zeyuzey0@gmail.com>
2025-10-09 11:58:28 +0800

    fix: resolve merge conflict UnitSeeder

--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -9,6 +9,20 @@ class UnitSeeder extends Seeder
 {
     public function run()
     {
+        Unit::factory()->count(5)->create();
+    }
+}
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Unit;
+
+class UnitSeeder extends Seeder
+{
+    public function run()
+    {
         // buat 5 unit
         Unit::factory()->count(5)->create();
     }
