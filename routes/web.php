3f5c6206ec241d5360388f71f9fcb7672b2e148c
Snowwxz <abizar300808@gmail.com>
2025-10-09 15:14:53 +0800

    Update Landing Page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
+});
+
+Route::get('/hari', function () {
+    return view('landing_hari');
+});
+
+Route::get('/tahun', function () {
+    return view('landing_tahun');
 });
