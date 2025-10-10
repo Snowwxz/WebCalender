91dd323f013f3eeeb8539682c976f82e00f11a6f
zalfa1717 <zalfaazizaa@gmail.com>
2025-10-10 10:54:36 +0800

    update button kembali

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a2e0e6ad3f.js" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
@@ -18,6 +19,14 @@
     <div class="login-container">
 
         <div class="left-section">
+            <div class="back-btn">
+                <a href="{{ url('/') }}" class="back-link">
+                    <i class="bi bi-arrow-left-short"></i>
+                    <span>Kembali</span>
+                </a>
+            </div>
+
+            <div class="form-wrapper">
             <div class="logo">
                 <img src="{{ asset('images/logo.png') }}" alt="Logo">
                 <div>
@@ -25,6 +34,7 @@
                     <small>Sistem Kalender Kota Samarinda</small>
                 </div>
             </div>
+        </div>
 
             <h3 class="fw-bold text-teal">Halo, Selamat Datang!</h3>
             <p class="text-muted mb-5">Silahkan Masuk ke akun Anda!</p>
@@ -73,4 +83,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
