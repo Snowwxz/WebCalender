2b1c2ae2252b4c273ff8543b2487f483969274de
yoricia22 <divasilviana30@gmail.com>
2025-10-17 16:18:49 +0800

    update code v2

--- database/migrations/2025_10_15_065144_add_start_end_time_to_agenda_table.php
+++ database/migrations/2025_10_15_065144_add_start_end_time_to_agenda_table.php
@@ -7,23 +7,23 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Tambah kolom baru ke tabel agenda
+     * Tambahkan kolom start_time dan end_time ke tabel agenda.
      */
     public function up(): void
     {
         Schema::table('agenda', function (Blueprint $table) {
-            $table->string('person_in_charge')->nullable()->after('description'); // setelah kolom description (ubah jika beda)
-            $table->string('involved_institution')->nullable()->after('person_in_charge');
+            $table->time('start_time')->nullable()->after('date');
+            $table->time('end_time')->nullable()->after('start_time');
         });
     }
 
     /**
-     * Hapus kolom jika rollback
+     * Hapus kolom jika rollback.
      */
     public function down(): void
     {
         Schema::table('agenda', function (Blueprint $table) {
-            $table->dropColumn(['person_in_charge', 'involved_institution']);
+            $table->dropColumn(['start_time', 'end_time']);
         });
     }
 };
