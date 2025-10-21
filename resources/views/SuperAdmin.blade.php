30a1e6820d531f94da2ffee4301e130163425c63
zalfa1717 <zalfaazizaa@gmail.com>
2025-10-21 11:00:19 +0800

    update sweetalert

--- resources/views/SuperAdmin.blade.php
+++ resources/views/SuperAdmin.blade.php
@@ -5,7 +5,7 @@ aaf<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Superadmin - SiKota</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/super-admin.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
