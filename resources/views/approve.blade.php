4a2ff6c2b2a1b3d83fb0952d61dd2bfe377aab03
Naeyla <zeyuzey0@gmail.com>
2025-10-29 10:33:35 +0800

    unit name

--- resources/views/approve.blade.php
+++ resources/views/approve.blade.php
@@ -73,8 +73,8 @@
                         </form>
                     </div>
                 </div>
-                    <div class="sort-container">
-                  <form method="GET" action="{{ route('approve') }}" class="sort-form">
+                <div class="sort-container">
+                    <form method="GET" action="{{ route('approve') }}" class="sort-form">
                         <input type="hidden" name="status" value="{{ request('status', 'all') }}">
                         <input type="hidden" name="q" value="{{ request('q') }}">
                         <select name="sort" onchange="this.form.submit()" class="sort-select">
@@ -94,7 +94,7 @@
                             </option>
                         </select>
                     </form>
-                    </div>
+                </div>
 
 
                 @if ($agendas->isEmpty())
@@ -197,15 +197,21 @@
                                 <div class="card-footer">
                                     <div class="submission-info">
                                         <i class="fas fa-user"></i>
-                                        <span>{{ $agenda->units ?? '-' }}</span>
-                                        <span class="submission-name">
-                                            {{ $agenda->person_in_charge ?? ($agenda->user->name ?? '-') }}
+                                        <span class="submission-unit">
+                                            {{ $agenda->unit->unit_name ?? ($agenda->units ?? '') }}
                                         </span>
+
+                                        @if ($agenda->unit || $agenda->units)
+                                            <span class="separator">&nbsp;–&nbsp;</span>
+                                        @endif
+
                                         <span class="submission-time">
-                                            &nbsp;Diajukan
+                                            Diajukan
                                             {{ \Carbon\Carbon::parse($agenda->created_at)->locale('id')->diffForHumans() }}
                                         </span>
                                     </div>
+
+
 
                                     @if ($agenda->status === 'pending')
                                         <div class="approval-actions">
