d4c0d096a009ca3eafb8f2ad679bbd505a6fa4b6
yoricia22 <divasilviana30@gmail.com>
2025-10-30 09:09:24 +0800

    save

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -271,7 +271,7 @@
     const message = localStorage.getItem('logoutSuccess');
     if (message) {
         showSuccessToast(message);
-        localStorage.removeItem('logoutSuccess'); // hapus supaya nggak muncul lagi nanti
+        localStorage.removeItem('logoutSuccess'); 
     }
 });
     </script>
