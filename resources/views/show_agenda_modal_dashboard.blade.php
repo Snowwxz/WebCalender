0e9ad8292df0fbff430b236b615f8095aa8dcd2c
Snowwxz <abizar300808@gmail.com>
2025-10-30 09:06:08 +0800

    fixing badge visibilitas show agenda modal dashboadrd

--- resources/views/show_agenda_modal_dashboard.blade.php
+++ resources/views/show_agenda_modal_dashboard.blade.php
@@ -73,7 +73,7 @@
 
                         <div class="mb-3">
                             <label class="text-muted small d-block mb-1">
-                                <i class="fas fa-eye"></i> Status
+                                <i class="fas fa-eye"></i> Visibilitas
                             </label>
                             <div class="fw-semibold" id="showAgendaAccess">-</div>
                         </div>
